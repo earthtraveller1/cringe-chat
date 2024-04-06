d7fd52e17636dcfb6e1edcfae6f834db200f01f6
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-04-06 14:06:15 -0400

    Created a basic Hello World server.

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main;
 
-import "fmt"
+import (
+    "net/http"
+    "log"
+    "os"
+)
+
+func indexHandler(pWriter http.ResponseWriter, pRequest *http.Request) {
+    http.ServeFile(pWriter, pRequest, "index.html")
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+    serverMux := http.NewServeMux()
+
+    serverMux.HandleFunc("/", indexHandler)
+
+    serverAddr := "0.0.0.0:6969"
+    if len(os.Args) > 2 {
+        serverAddr = os.Args[1]
+    }
+
+    server := http.Server {
+        Addr: serverAddr,
+        Handler: serverMux,
+    }
+
+    log.Printf("Listening at %s...\n", serverAddr)
+    server.ListenAndServe()
 }
